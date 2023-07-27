48c916920016c0e88cc2b92ff558f9a80ed11677
Charles Holbrow <CharlesHolbrow@gmail.com>
2023-07-27 17:43:34 -0400

    initial job_jamendo package and local environment

---
+++ job_jamendo_copy/README.md
@@ -0,0 +1,39 @@
+# job_jamendo_copy
+initial job for copying+triming jamendo dataset
+
+# Development
+## Quick Start
+Install dependencies (we highly recommend creating and activating a virtual python environment first):
+```sh
+pip install [-e] '.[code-style, type-check, tests]'
+```
+
+## Dependencies
+### conda
+We use `pip` to handle python dependencies.  To create or update an environment:
+
+```sh
+pip install [-e] '.[code-style, type-check, tests]'
+```
+
+All dependencies are listed in the `pyproject.toml` file in the 'dependencies' section.
+
+## Code Quality
+### Testing
+We use `pytest` for testing, there's no coverage target at the moment but essential functions and custom logic should definitely be tested. To run the tests:
+```sh
+make tests
+```
+
+### Code Style
+We use `flake8` for linting and `black` for formatting.
+
+```sh
+make code-style
+```
+
+### Static Typing
+We check static types using `mypy`.
+```sh
+make type-check
+```
---
+++ job_jamendo_copy/tests/test_paths.py
@@ -0,0 +1,104 @@
+from job_jamendo_copy.paths import get_target_path
+import pathlib
+import pytest
+
+
+def test_get_target_path_basic():
+    source_dir = "gs://klay-datasets/mtg_jamendo_autotagging/audios/"
+    target_dir = "gs://klay-datasets/mtg_jamendo/"
+
+    source_uris = [
+        "gs://klay-datasets/mtg_jamendo_autotagging/audios/00/1009600.mp3",
+        "gs://klay-datasets/mtg_jamendo_autotagging/audios/00/1012000.mp3",
+        "gs://klay-datasets/mtg_jamendo_autotagging/audios/01/1009601.mp3",
+        "gs://klay-datasets/mtg_jamendo_autotagging/audios/01/1009701.mp3",
+    ]
+    target_uris = [
+        "gs://klay-datasets/mtg_jamendo/00/1009600.source.wav",
+        "gs://klay-datasets/mtg_jamendo/00/1012000.source.wav",
+        "gs://klay-datasets/mtg_jamendo/01/1009601.source.wav",
+        "gs://klay-datasets/mtg_jamendo/01/1009701.source.wav",
+    ]
+
+    for source, expected in zip(source_uris, target_uris):
+        actual_output_filename = get_target_path(source, source_dir, target_dir)
+        assert actual_output_filename == expected
+
+
+def test_get_target_path_errors():
+    target_dir = "gs://klay-datasets/mtg_jamendo/"
+
+    # When source file is not in the source_dir, raise an error
+    erroneous_source_dir = "gs://klay-datasets/ELSEWHERE"
+    source_uri = "gs://klay-datasets/mtg_jamendo_autotagging/audios/00/1009600.mp3"
+    with pytest.raises(ValueError):
+        get_target_path(source_uri, erroneous_source_dir, target_dir)
+
+    # The following are different paths
+    # 1. "/klay-datasets/"
+    # 2. "gs://klay-datasets/"
+    erroneous_source_dir = "/klay-datasets/mtg_jamendo_autotagging/audios/"
+    source_uri = "gs://klay-datasets/mtg_jamendo_autotagging/audios/00/1009600.mp3"
+    with pytest.raises(ValueError):
+        get_target_path(source_uri, erroneous_source_dir, target_dir)
+
+
+def test_get_target_path_gcp_to_local():
+    source_dir = "gs://klay-datasets/mtg_jamendo_autotagging/audios/"
+    target_dir = "/klay-datasets/mtg_jamendo"
+
+    source_uris = [
+        "gs://klay-datasets/mtg_jamendo_autotagging/audios/00/1009600.mp3",
+        "gs://klay-datasets/mtg_jamendo_autotagging/audios/00/1012000.mp3",
+        "gs://klay-datasets/mtg_jamendo_autotagging/audios/01/1009601.mp3",
+        "gs://klay-datasets/mtg_jamendo_autotagging/audios/01/1009701.mp3",
+    ]
+    target_uris = [
+        "/klay-datasets/mtg_jamendo/00/1009600.source.wav",
+        "/klay-datasets/mtg_jamendo/00/1012000.source.wav",
+        "/klay-datasets/mtg_jamendo/01/1009601.source.wav",
+        "/klay-datasets/mtg_jamendo/01/1009701.source.wav",
+    ]
+
+    for source, expected in zip(source_uris, target_uris):
+        actual_output_filename = get_target_path(source, source_dir, target_dir)
+        assert actual_output_filename == expected
+
+def test_get_target_path_local_to_gcp():
+    source_dir = "/klay-datasets/mtg_jamendo_autotagging/audios/"
+    target_dir = "gs://klay-datasets/mtg_jamendo/"
+
+    source_uris = [
+        "/klay-datasets/mtg_jamendo_autotagging/audios/00/1009600.mp3",
+        "/klay-datasets/mtg_jamendo_autotagging/audios/00/1012000.mp3",
+        "/klay-datasets/mtg_jamendo_autotagging/audios/01/1009601.mp3",
+        "/klay-datasets/mtg_jamendo_autotagging/audios/01/1009701.mp3",
+    ]
+    target_uris = [
+        "gs://klay-datasets/mtg_jamendo/00/1009600.source.wav",
+        "gs://klay-datasets/mtg_jamendo/00/1012000.source.wav",
+        "gs://klay-datasets/mtg_jamendo/01/1009601.source.wav",
+        "gs://klay-datasets/mtg_jamendo/01/1009701.source.wav",
+    ]
+
+    for source, expected in zip(source_uris, target_uris):
+        actual_output_filename = get_target_path(source, source_dir, target_dir)
+        assert actual_output_filename == expected
+
+def test_pathlib_relative_to():
+    # Ensure that pathlib.Path.relative_to can handle both paths and URIs
+    uri = "gs://klay-datasets/mtg_jamendo_autotagging/audios/00/1009600.mp3"
+    result1 = pathlib.PurePosixPath(uri).relative_to("gs://klay-datasets/mtg_jamendo_autotagging/audios/")
+    assert result1 == pathlib.PurePosixPath("00/1009600.mp3")
+    result2 = pathlib.PurePosixPath(uri).relative_to("gs://klay-datasets/mtg_jamendo_autotagging/audios")
+    assert result2 == pathlib.PurePosixPath("00/1009600.mp3")
+
+    filepath = "/klay-datasets/mtg_jamendo_autotagging/audios/00/1009600.mp3"
+    result3 = pathlib.PurePosixPath(filepath).relative_to("/klay-datasets/mtg_jamendo_autotagging/audios/")
+    assert result3 == pathlib.PurePosixPath("00/1009600.mp3")
+    result4 = pathlib.PurePosixPath(filepath).relative_to("/klay-datasets/mtg_jamendo_autotagging/audios")
+    assert result4 == pathlib.PurePosixPath("00/1009600.mp3")
+
+    # path_as_string = "gs://klay-datasets/mtg_jamendo_autotagging/audios/00/1009600.mp3"
+    # path1 = pathlib.PurePosixPath(path_as_string)
+    # assert (str(path1) == path_as_string)
