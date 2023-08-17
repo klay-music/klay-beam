36f0ddc2e04eb82d8223a93387b803948a4c026e
Charles Holbrow <CharlesHolbrow@gmail.com>
2023-08-17 00:13:21 -0400

    Bump cuda docker version

--- tag-klay-beam-demucs-cuda.sh
+++ tag-klay-beam-demucs-cuda.sh
@@ -2,7 +2,7 @@
 set -e
 set -x
 
-VERSION=0.7.0-demucs-cuda-rc.3
+VERSION=0.8.0-demucs-cuda
 docker tag klay-beam:demucs-cuda us-docker.pkg.dev/klay-home/klay-docker/klay-beam:$VERSION
 docker tag klay-beam:demucs-cuda us-docker.pkg.dev/klay-home/klay-docker/klay-beam:latest
 
