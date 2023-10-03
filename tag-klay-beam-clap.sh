d985ad4f867f61ffa90ea1fc8e843283246b3090
Max Kraan <17745834+mxkrn@users.noreply.github.com>
2023-10-03 10:54:50 +0000

    tag-klay-beam-clap.sh

---
+++ tag-klay-beam-clap.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+set -x
+
+VERSION=0.10.2-clap
+docker tag klay-beam:nac us-docker.pkg.dev/klay-home/klay-docker/klay-beam:latest
+docker tag klay-beam:nac us-docker.pkg.dev/klay-home/klay-docker/klay-beam:$VERSION
+
+docker push us-docker.pkg.dev/klay-home/klay-docker/klay-beam:latest
+docker push us-docker.pkg.dev/klay-home/klay-docker/klay-beam:$VERSION
