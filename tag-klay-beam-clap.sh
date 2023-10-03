bf50bb94a1697a56352647c95e7f968f87e2513e
Max Kraan <17745834+mxkrn@users.noreply.github.com>
2023-10-03 18:06:46 +0200

    fix typo in tag-klay-beam-clap.sh

--- tag-klay-beam-clap.sh
+++ tag-klay-beam-clap.sh
@@ -3,8 +3,8 @@ set -e
 set -x
 
 VERSION=0.10.2-clap
-docker tag klay-beam:nac us-docker.pkg.dev/klay-home/klay-docker/klay-beam:latest
-docker tag klay-beam:nac us-docker.pkg.dev/klay-home/klay-docker/klay-beam:$VERSION
+docker tag klay-beam:clap us-docker.pkg.dev/klay-home/klay-docker/klay-beam:latest
+docker tag klay-beam:clap us-docker.pkg.dev/klay-home/klay-docker/klay-beam:$VERSION
 
 docker push us-docker.pkg.dev/klay-home/klay-docker/klay-beam:latest
 docker push us-docker.pkg.dev/klay-home/klay-docker/klay-beam:$VERSION
