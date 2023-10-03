fd612b8a2313ad3ef89a5df2f268dd4e1ac65642
Max Kraan <17745834+mxkrn@users.noreply.github.com>
2023-10-03 20:50:36 +0200

    update klay_beam to 0.10.3

--- tag-klay-beam-clap.sh
+++ tag-klay-beam-clap.sh
@@ -2,7 +2,7 @@
 set -e
 set -x
 
-VERSION=0.10.2-clap
+VERSION=0.10.3-clap
 docker tag klay-beam:clap us-docker.pkg.dev/klay-home/klay-docker/klay-beam:latest
 docker tag klay-beam:clap us-docker.pkg.dev/klay-home/klay-docker/klay-beam:$VERSION
 
