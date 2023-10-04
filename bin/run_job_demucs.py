83f12d745b4ce55d38e962f30c82af535d500e3e
Charles Holbrow <CharlesHolbrow@gmail.com>
2023-10-04 18:43:54 -0400

    launch: demucs-046-on-full-length-jamendo-all

--- bin/run_job_demucs.py
+++ bin/run_job_demucs.py
@@ -93,7 +93,7 @@ def run():
                     new_suffix=[".bass.wav", ".drums.wav", ".other.wav", ".vocals.wav"],
                     source_dir=known_args.input,
                     target_dir=known_args.output,
-                    check_timestamp=True,
+                    check_timestamp=False,
                 )
             )
             # ReadMatches produces a PCollection of ReadableFile objects
