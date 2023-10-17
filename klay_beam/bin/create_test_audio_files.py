6fb5025f5f6e7bd82f9e5b2c5ce315b020808630
Charles Holbrow <CharlesHolbrow@gmail.com>
2023-10-17 14:41:36 -0400

    move script for creating test audio files into klay_beam/bin

--- klay_beam/bin/create_test_audio_files.py
+++ klay_beam/bin/create_test_audio_files.py
@@ -11,18 +11,22 @@ def create_test_mono_audio_channel(fade_in_seconds: float = 1.0, sine=False):
     to be in a multi-channel format, (i.e. mono like this: `[[0, 0, 0, ...]]`).
     """
     sr = 44100
-    seconds = 2
-    total_samples = int(sr * seconds)
+
+    total_seconds = 0.5
+    total_samples = int(sr * total_seconds)
+
+    fade_in_seconds = 0.25
+    fade_in_samples = int(sr * fade_in_seconds)
+    fade_in_samples = min(total_samples, fade_in_samples)
+
     audio_channel = np.random.rand(total_samples) * 2 - 1
 
     if sine:
         sine_frequency = 1000
-        t = np.linspace(0, seconds, total_samples, endpoint=False)
+        t = np.linspace(0, total_seconds, total_samples, endpoint=False)
         audio_channel = np.sin(2 * np.pi * sine_frequency * t)
 
-    fade_in_samples = min(total_samples, int(sr * fade_in_seconds))
     fade_in = np.linspace(0.0, 1.0, fade_in_samples) ** 2.0
-
     audio_channel[:fade_in_samples] *= fade_in
 
     return audio_channel, sr
@@ -53,14 +57,14 @@ def run():
     mono_audio, sr = create_test_audio_mono()
     mp3_buffer = numpy_to_mp3(mono_audio, sr)
 
-    with open("test_audio/test_mono.mp3", "wb") as out_file:
+    with open("tests/test_data/audio/test_mono.mp3", "wb") as out_file:
         out_file.write(mp3_buffer.getvalue())
         mp3_buffer.seek(0)
 
     stereo_audio, sr = create_test_audio_stereo()
     mp3_buffer = numpy_to_mp3(stereo_audio, sr)
 
-    with open("test_audio/test_stereo.mp3", "wb") as out_file:
+    with open("tests/test_data/audio/test_stereo.mp3", "wb") as out_file:
         out_file.write(mp3_buffer.getvalue())
         mp3_buffer.seek(0)
 
@@ -74,25 +78,25 @@ def run():
         found_error
     ), "Expected an assertion error when passing 4 channel audio to numpy_to_mp3"
 
-    with open("test_audio/test_stereo_16bit.wav", "wb") as out_file:
+    with open("tests/test_data/audio/test_stereo_16bit.wav", "wb") as out_file:
         out_file.write(numpy_to_wav(stereo_audio, sr, bit_depth=16).getvalue())
 
-    with open("test_audio/test_stereo_24bit.wav", "wb") as out_file:
+    with open("tests/test_data/audio/test_stereo_24bit.wav", "wb") as out_file:
         out_file.write(numpy_to_wav(stereo_audio, sr, bit_depth=24).getvalue())
 
-    with open("test_audio/test_stereo_32bit.wav", "wb") as out_file:
+    with open("tests/test_data/audio/test_stereo_32bit.wav", "wb") as out_file:
         out_file.write(numpy_to_wav(stereo_audio, sr, bit_depth=32).getvalue())
 
-    with open("test_audio/test_stereo_64bit.wav", "wb") as out_file:
+    with open("tests/test_data/audio/test_stereo_64bit.wav", "wb") as out_file:
         out_file.write(numpy_to_wav(stereo_audio, sr, bit_depth=64).getvalue())
 
-    with open("test_audio/test_quad.wav", "wb") as out_file:
+    with open("tests/test_data/audio/test_quad.wav", "wb") as out_file:
         out_file.write(numpy_to_wav(four_channel_audio, sr).getvalue())
 
-    with open("test_audio/test_stereo.ogg", "wb") as out_file:
+    with open("tests/test_data/audio/test_stereo.ogg", "wb") as out_file:
         out_file.write(numpy_to_ogg(stereo_audio, sr, safe=False).getvalue())
 
-    with open("test_audio/test_quad.ogg", "wb") as out_file:
+    with open("tests/test_data/audio/test_quad.ogg", "wb") as out_file:
         out_file.write(numpy_to_ogg(four_channel_audio, sr, safe=False).getvalue())
 
 
