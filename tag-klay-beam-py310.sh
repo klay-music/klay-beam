b889ca18e5b7e00d3a92960c4d3c0e341541bb47
Charles Holbrow <CharlesHolbrow@gmail.com>
2023-10-18 22:40:01 -0400

    job-organization: Dockerfile.conda can generically build for any .lock file

--- tag-klay-beam-py310.sh
+++ tag-klay-beam-py310.sh
@@ -2,7 +2,7 @@
 set -e
 set -x
 
-VERSION=0.10.0-py310
+VERSION=0.11.0-py310-torch
 docker tag klay-beam:py310 us-docker.pkg.dev/klay-home/klay-docker/klay-beam:latest
 docker tag klay-beam:py310 us-docker.pkg.dev/klay-home/klay-docker/klay-beam:$VERSION
 
