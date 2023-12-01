ac9b0c0b86bdcddf314008308c5de6721a258029
Charles Holbrow <CharlesHolbrow@gmail.com>
2023-12-01 14:28:19 -0500

    refactor: removing all job_ dirs (they will be managed in the dedicated klay-beam-jobs repository)

--- job_adt/fetch-checkpoint.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-rm -rf assets; mkdir -p assets/e-gmd_checkpoint; cd assets/e-gmd_checkpoint
-curl -LO https://storage.googleapis.com/magentadata/models/onsets_frames_transcription/e-gmd_checkpoint.zip
-unzip e-gmd_checkpoint.zip; rm e-gmd_checkpoint.zip
--- job_adt/src/job_adt/__init__.py
+++
@@ -1,3 +0,0 @@
-import os
-
-__version__ = "0.5.3"
