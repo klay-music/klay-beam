35e4184cb549cd8533e548733e7a6d9df9d35348
Charles Holbrow <CharlesHolbrow@gmail.com>
2023-12-01 12:58:07 -0500

    klay_beam: Add DeprecationWarning to MultiMatchFiles
    Instead, use: beam.Create([p1, p1]) | beam.io.fileio.MatchAll()

--- klay_beam/tests/test_multi_match_files.py
+++ klay_beam/tests/test_multi_match_files.py
@@ -25,3 +25,37 @@ def test_MultiMatchFiles():
 
         # Check if the results match the expected file names
         assert_that(file_names, equal_to(expected))
+
+
+from apache_beam.io.fileio import MatchAll
+
+def test_apache_beam_MatchAll():
+    """Klay_beam version 0.12.x has a custom transform called MultiMatchFiles
+    used to match multiple file patterns. However, this method is slow, because
+    it could not parallelize.
+
+    This test documents a better way to do this using Beam's MatchAll transform.
+
+    See:
+    https://beam.apache.org/releases/pydoc/current/apache_beam.io.fileio.html#apache_beam.io.fileio.MatchAll
+
+    klay_beam.transforms.MultiMatchFiles should be deprecated after version
+    0.12.x.
+    """
+    this_file = Path(__file__)
+    data_dir = this_file.parent / "test_data"
+    patterns = [str(data_dir / fn) for fn in ["00/*.txt", "01/*.txt"]]
+    expected = [
+        str(data_dir / fn) for fn in ["00/a.txt", "00/b.txt", "01/a.txt", "01/b.txt"]
+    ]
+
+    with BeamTestPipeline() as p:
+        results = p | beam.Create(patterns) | MatchAll()
+
+        # Extract file names from the metadata for assertion
+        file_names = results | "Extract File Names" >> beam.Map(
+            lambda metadata: metadata.path
+        )
+
+        # Check if the results match the expected file names
+        assert_that(file_names, equal_to(expected))
