ad29562d17deb146b853b17a8ff05a41d6e6404f
Max Kraan <17745834+mxkrn@users.noreply.github.com>
2024-04-04 12:32:04 +0000

    remove broken checks for torch and torchaudio version

--- klay_beam/src/klay_beam/torch_utils.py
+++ klay_beam/src/klay_beam/torch_utils.py
@@ -7,16 +7,6 @@ def torch_check() -> Tuple[bool, Optional[ImportError]]:
     try:
         import torch
         import torchaudio
-
-        if torch.__version__ < "1.0.0":
-            raise ImportError(
-                "Incompatible version of torch is installed. Install version 1.0.0 or newer."
-            )
-        if torchaudio.__version__ < "0.8.0":
-            raise ImportError(
-                "Incompatible version of torchaudio is installed. Install version 0.8.0 or newer."
-            )
-
         torch_available = True
     except ImportError as e:
         torch_import_error = e
