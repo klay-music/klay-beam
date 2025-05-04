8c8fec47b8f650a0ab9241056913755c02d06f78
Max Kraan <17745834+mxkrn@users.noreply.github.com>
2025-05-05 00:06:24 +0200

    job_demucs: Update version to 0.3.0 in .env and __init__.py.

--- jobs/job_demucs/src/job_demucs/__init__.py
+++ jobs/job_demucs/src/job_demucs/__init__.py
@@ -1,6 +1,4 @@
-import os
-
-__version__ = "0.2.4"
+__version__ = "0.3.0"
 
 # IMPORTANT: Prevent submitit package from silencing Dataflow logs!
 #
@@ -8,4 +6,5 @@ __version__ = "0.2.4"
 # submitit has an incompatibility with GCP Dataflow. To prevent submitit from
 # interfering with logging, set this environment variable. See:
 # https://github.com/facebookincubator/submitit/blob/4cf1462d7216f9dcc530daeb703ce07c37cf9d72/submitit/core/logger.py#L11-L16
+import os
 os.environ["SUBMITIT_LOG_LEVEL"] = "NOCONFIG"
