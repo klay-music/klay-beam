158336856dc45f595da36120d34ce2b0e936aad4
Björn Winckler <bjorn.winckler@gmail.com>
2025-05-14 21:04:37 +0100

    Add --features flag to select which features to use.
    All possible features are listed in FEATURE_SUFFIX:
    
    *  audioset_yamnet (filters music, no output added)
    *  klaynacvae
    *  mtrpp
    *  whisper_byt5
    *  whisper

--- jobs/job_mds/src/job_mds/transforms.py
+++ jobs/job_mds/src/job_mds/transforms.py
@@ -1,6 +1,4 @@
 import apache_beam as beam
-from apache_beam.coders import VarIntCoder
-from apache_beam.transforms import userstate
 from dataclasses import dataclass
 from typing import Any
 import numpy as np
@@ -12,11 +10,49 @@ from streaming import MDSWriter
 import logging
 import os
 
-from klay_data import pipeline
+from klay_data.pipeline import (
+    Pipeline,
+    ValidateAudiosetYamnet,
+    FilterIsMusic,
+    ValidateKlayNACVAE,
+    FilterLength,
+    ValidateMTRPP,
+    MaybeSetFeature,
+    WrapFeature,
+    FlattenFeature,
+    WhisperLyricsInfo,
+    run as run_pipeline,
+)
 
 
+# Define all the suffixes for the features that may be included in the input.
+FEATURE_SUFFIX = {
+    "audioset_yamnet": ".source.audioset_yamnet.npy",
+    "klaynacvae": ".source.klaynacvae-0.6.2.npy",
+    "mtrpp": ".source.mtrpp.npy",
+    "whisper_byt5": ".vocals.whisper-byt5.npz",
+    "whisper": ".vocals.whisper.json",
+}
+
+
+# Define all the columns that may be included in the MDS output. Note that some features have
+# multiple columns, which is indicated by the "." in the key.
+FEATURE_MDS_TYPE = {
+    "klaynacvae": "ndarray:float32",
+    "mtrpp": "ndarray:float32",
+    "whisper_byt5.byt5_embeds": "ndarray:float32",
+    "whisper_byt5.byt5_tokens": "ndarray:int32",
+    "whisper_byt5.ends": "ndarray:float32",
+    "whisper_byt5.starts": "ndarray:float32",
+    "whisper": "json",
+}
+
+
+# NOTE: This class is defined here because we don't want the Beam dependency inside klay-data.
 @dataclass(kw_only=True)
 class LoadFeatureBeam:
+    """Load a feature in a Beam pipeline."""
+
     input_key: str
     output_key: str
 
@@ -50,54 +86,91 @@ class LoadFeatureBeam:
 
 
 class ProcessURI(beam.DoFn):
-    def __init__(self, min_duration: int, frame_rate: int) -> None:
+    def __init__(
+        self, min_duration: int, frame_rate: int, suffixes: dict[str, str]
+    ) -> None:
         self.min_duration = min_duration
         self.frame_rate = frame_rate
         self.min_len = math.ceil(min_duration * self.frame_rate)
+        self.suffixes = suffixes
         self.uri_processing_errors = beam.metrics.Metrics.counter(
             ProcessURI.__name__, "uri_processing_errors"
         )
         self.uri_processed_successfully = beam.metrics.Metrics.counter(
             ProcessURI.__name__, "uri_processed_successfully"
         )
-        self.p = pipeline.Pipeline(
-            # Ensure audio is music.
+
+        stages = {}
+
+        # Ensure audio is music.
+        stages["audioset_yamnet"] = Pipeline(
             LoadFeatureBeam(
                 input_key="audioset_yamnet.path", output_key="audioset_yamnet"
             ),
-            pipeline.ValidateAudiosetYamnet(input_key="audioset_yamnet", feat_dim=521),
-            pipeline.FilterIsMusic(input_key="audioset_yamnet"),
-            # Load KlayNACVAE features.
-            LoadFeatureBeam(input_key="klaynacvae.path", output_key="klaynacvae"),
-            pipeline.ValidateKlayNACVAE(input_key="klaynacvae", feat_dim=128),
-            pipeline.FilterLength(input_key="klaynacvae", min_len=self.min_len),
-            # Load MTR++ features.
-            LoadFeatureBeam(input_key="mtrpp.path", output_key="mtrpp"),
-            pipeline.ValidateMTRPP(input_key="mtrpp", feat_dim=128),
-            # Optionally load Whisper features.
-            pipeline.Pipeline(
-                LoadFeatureBeam(input_key="whisper.path", output_key="whisper"),
-                is_optional=True,
-                # pipeline.WrapFeature(input_key="whisper", dataclass=pipeline.WhisperLyricsInfo),
-            ),
-            pipeline.MaybeSetFeature(input_key="whisper", value={}),
-            # pipeline.MaybeAddMissingWhisperLyrics(input_key="whisper"),
-            # pipeline.FlattenFeature(input_key="whisper", output_prefix="whisper.")
+            ValidateAudiosetYamnet(input_key="audioset_yamnet", feat_dim=521),
+            FilterIsMusic(input_key="audioset_yamnet"),
         )
+
+        # Load KlayNACVAE features.
+        stages["klaynacvae"] = Pipeline(
+            LoadFeatureBeam(input_key="klaynacvae.path", output_key="klaynacvae"),
+            ValidateKlayNACVAE(input_key="klaynacvae", feat_dim=128),
+            FilterLength(input_key="klaynacvae", min_len=self.min_len),
+        )
+
+        # Load MTR++ features.
+        stages["mtrpp"] = Pipeline(
+            LoadFeatureBeam(input_key="mtrpp.path", output_key="mtrpp"),
+            ValidateMTRPP(input_key="mtrpp", feat_dim=128),
+        )
+
+        # Optionally load Whisper features.
+        stages["whisper"] = Pipeline(
+            Pipeline(
+                LoadFeatureBeam(input_key="whisper.path", output_key="whisper"),
+                # TODO: Validate Whisper features.
+                is_optional=True,
+            ),
+            MaybeSetFeature(input_key="whisper", value={}),
+        )
+
+        # Optionally load Whisper ByT5 features.
+        stages["whisper_byt5"] = Pipeline(
+            Pipeline(
+                LoadFeatureBeam(
+                    input_key="whisper_byt5.path", output_key="whisper_byt5"
+                ),
+                # TODO: Validate Whisper ByT5 features.
+                WrapFeature(input_key="whisper_byt5", dataclass=WhisperLyricsInfo),
+                is_optional=True,
+            ),
+            MaybeSetFeature(
+                input_key="whisper_byt5",
+                value=WhisperLyricsInfo(
+                    starts=np.zeros((1,), dtype=np.float32),
+                    ends=np.zeros((1,), dtype=np.float32),
+                    byt5_tokens=np.full((1, 1), -1, dtype=np.int32),
+                    byt5_embeds=np.zeros((1, 1), dtype=np.float32),
+                ),
+            ),
+            FlattenFeature(input_key="whisper_byt5", output_prefix="whisper_byt5."),
+        )
+
+        # NOTE! This code is carefully written so that the order of stages in `self.pipeline` is the
+        # same as the order defined in the code above. In particular, `include_stage` is the same
+        # order as `stages``. This is essential because, e.g. music filtering should happen first.
+        include_stage = {k: k in self.suffixes for k in stages}
+        self.pipeline = Pipeline(*[v for k, v in stages.items() if include_stage[k]])
 
     def process(self, uri: str, *_):
         try:
+            # Construct paths to features. These are loaded by LoadFeatureBeam stages.
             basename = os.path.basename(uri)
-
-            # Construct the data dictionary with full paths
             data = {
-                "whisper.path": f"{uri}/{basename}.vocals.whisper.json",
-                "mtrpp.path": f"{uri}/{basename}.source.mtrpp.npy",
-                "klaynacvae.path": f"{uri}/{basename}.source.klaynacvae-0.6.2.npy",
-                "audioset_yamnet.path": f"{uri}/{basename}.source.audioset_yamnet.npy",
+                k + ".path": f"{uri}/{basename}{v}" for k, v in self.suffixes.items()
             }
 
-            result = pipeline.run(self.p, data=data)
+            result = run_pipeline(self.pipeline, data=data)
             self.uri_processed_successfully.inc()
             yield result
         except Exception as e:
@@ -106,16 +179,12 @@ class ProcessURI(beam.DoFn):
 
 
 class WriteMDS(beam.DoFn):
-    def __init__(self, dest_dir: str) -> None:
+    def __init__(self, dest_dir: str, features: list[str]) -> None:
         self.dest_dir = dest_dir
+        # The MDS types have keys that are either feature names or feature names with a "." and a
+        # suffix, so make sure to include both in the MDS output.
         self.columns = {
-            "klaynacvae": "ndarray:float32",
-            # "whisper.starts": "ndarray:float32",
-            # "whisper.ends": "ndarray:float32",
-            # "whisper.byt5_tokens": "ndarray:int32",
-            # "whisper.byt5_embeds": "ndarray:float32",
-            "whisper": "json",
-            "mtrpp": "ndarray:float32",
+            k: v for k, v in FEATURE_MDS_TYPE.items() if k.split(".")[0] in features
         }
         self.writer = None
         self.worker_id = None
@@ -139,43 +208,3 @@ class WriteMDS(beam.DoFn):
 
     def teardown(self):
         self.writer.finish()
-
-
-class _EnumerateDoFn(beam.DoFn):
-    """
-    Adds a monotonically-increasing integer to every element.
-    Works in streaming or batch, on DirectRunner and Dataflow.
-    """
-
-    # One VALUE state cell per key, initialised to 0
-    COUNTER = userstate.ReadModifyWriteStateSpec("c", VarIntCoder())
-
-    def __init__(self, start: int = 0):
-        self._start = start
-
-    def process(
-        self,
-        kv,
-        counter_state=beam.DoFn.StateParam(COUNTER),
-    ):
-        # kv comes from (None, element)
-        _, element = kv
-
-        current = counter_state.read() or self._start
-        counter_state.write(current + 1)  # persist for the next element
-
-        yield current, element
-
-
-class Enumerate(beam.PTransform):
-    """(e1, e2, …) → (0, e1), (1, e2), …"""
-
-    def __init__(self, start: int = 0):
-        self._start = start
-
-    def expand(self, pcoll):
-        return (
-            pcoll
-            | "KeyByNone" >> beam.Map(lambda x: (None, x))
-            | "AttachIndex" >> beam.ParDo(_EnumerateDoFn(self._start))
-        )
